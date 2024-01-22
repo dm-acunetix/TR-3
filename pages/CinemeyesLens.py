b7ba243963ec43764103c06a1080884d26f5ad85
PrasannaGH963 <prasannabadgujar963@gmail.com>
2024-01-22 22:05:08 +0530

    updates in CinemeyesLens

--- pages/CinemeyesLens.py
+++ pages/CinemeyesLens.py
@@ -42,57 +42,57 @@ if search_id:
 
             st.write(f"Event for ID {search_id}:")
             st.write(game_list)
-            
-            # st.info("Team should at least have 2 teammates")
-            with st.form("Team_Reg"):
-                teammate_info_list = []
 
-                for game in game_list:
-                    st.title(f"Teammates for: {game}")
+            if game_str == 'Photography':
+                st.error("Solo entries only for Photography")
+                gif_url = "https://i.ibb.co/nnQBz2x/SOLO.gif"  # Replace with your GIF URL
+                st.image(gif_url, caption="Embark on your solo adventure!", use_column_width=True)
+            else:
+                with st.form("Team_Reg"):
+                    teammate_info_list = []
 
-                    if game == 'Photography':
-                        st.error("Solo entries only for Photography")
-                        gif_url = "https://i.ibb.co/nnQBz2x/SOLO.gif"  # Replace with your GIF URL
-                        st.image(gif_url, caption="Embark on your solo adventure!", use_column_width=True)
-                        num = 0  # Set num to 0 for Photography
-                    else:
-                        num = 1
+                    for game in game_list:
+                        st.title(f"Teammates for: {game}")
+                        if game == 'Reel':
+                            num = 1 
+                        else:
+                            num=1  # Set num to 1 for Reel
 
-                    teammate_names = []
-                    teammate_numbers = []
-                    teammate_emails = []
+                        teammate_names = []
+                        teammate_numbers = []
+                        teammate_emails = []
 
-                    for i in range(num):
-                        teammate_name = st.text_input(f"Name of Teammate {i + 1}:", key=f"{game}_name_{i}")
-                        teammate_number = st.text_input(f"Contact no. of Teammate {i + 1}:", max_chars=10,
-                                                        key=f"{game}_num_{i}")
-                        teammate_email = st.text_input(f"Email of Teammate {i + 1}:", key=f"{game}_em_{i}")
+                        for i in range(num):
+                            teammate_name = st.text_input(f"Name of Teammate {i + 1}:", key=f"{game}_name_{i}")
+                            teammate_number = st.text_input(f"Contact no. of Teammate {i + 1}:", max_chars=10,
+                                                            key=f"{game}_num_{i}")
+                            teammate_email = st.text_input(f"Email of Teammate {i + 1}:", key=f"{game}_em_{i}")
 
-                        teammate_names.append(teammate_name)
-                        teammate_numbers.append(teammate_number)
-                        teammate_emails.append(teammate_email)
+                            teammate_names.append(teammate_name)
+                            teammate_numbers.append(teammate_number)
+                            teammate_emails.append(teammate_email)
 
-                    # Concatenate teammate info with commas
-                    teammate_info = {
-                        'ID': search_id,
-                        'For Event': game,
-                        'Names': ', '.join(teammate_names),
-                        'Numbers': ', '.join(teammate_numbers),
-                        'Emails': ', '.join(teammate_emails)
-                    }
+                        # Concatenate teammate info with commas
+                        teammate_info = {
+                            'ID': search_id,
+                            'For Event': game,
+                            'Names': ', '.join(teammate_names),
+                            'Numbers': ', '.join(teammate_numbers),
+                            'Emails': ', '.join(teammate_emails)
+                        }
 
-                    teammate_info_list.append(teammate_info)
+                        teammate_info_list.append(teammate_info)
 
-                submit_button = st.form_submit_button("Submit")
-                if submit_button:
-                    df = pd.DataFrame(teammate_info_list)
-                    print(teammate_info_list)
-                    w = client.open_by_key("1VeWt6NBUGqc_4TldxqFfrw9qWhd_4n_FKM0H0XEvoLw").worksheet("CML TR")
-                    last = len(w.col_values(1)) + 1
-                    set_with_dataframe(w, df, row=last, include_index=False, include_column_header=False)
-                    st.success("Team Registered! ✨")
-                    st.dataframe(df)
-                    st.info("Thank You")
+                    submit_button = st.form_submit_button("Submit")
+                    if submit_button:
+                        df = pd.DataFrame(teammate_info_list)
+                        print(teammate_info_list)
+                        w = client.open_by_key("1VeWt6NBUGqc_4TldxqFfrw9qWhd_4n_FKM0H0XEvoLw").worksheet("CML TR")
+                        last = len(w.col_values(1)) + 1
+                        set_with_dataframe(w, df, row=last, include_index=False, include_column_header=False)
+                        st.success("Team Registered! ✨")
+                        st.dataframe(df)
+                        st.info("Thank You")
 
         else:
             st.warning("ID not found")
